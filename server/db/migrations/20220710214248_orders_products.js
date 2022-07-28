6a0b9c1132c6eaffff0ff2b3a85369bf9f40acd8
falicitt <falicity@qq.com>
2022-07-29 11:56:06 +1200

    fix bugs

--- server/db/migrations/20220710214248_orders_products.js
+++ server/db/migrations/20220710214248_orders_products.js
@@ -2,18 +2,18 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
+exports.up = function (knex) {
   return knex.schema.createTable('orders_products', (table) => {
-    table.integer('product_id').references('products.id')
-    table.integer('order_id').references('orders.id')
+    table.integer('product_id')
+    table.integer('order_id')
     table.integer('quantity')
   })
-};
+}
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
+exports.down = function (knex) {
   return knex.schema.dropTable('orders_products')
-};
+}
