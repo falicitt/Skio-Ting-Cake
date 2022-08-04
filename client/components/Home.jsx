bebdb11a267f90a638159633d9a96cb996179dea
falicitt <falicity@qq.com>
2022-08-04 15:32:04 +1200

    fix useeffect

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -11,7 +11,7 @@ function Home () {
     getCakes()
       .then(cakesArr => setCakes(cakesArr))
       .catch(err => console.log('getcakeserror', err))
-  }, [])
+  }, [cakes])
 
   return (
     <>
