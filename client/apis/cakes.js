d7cd260a8e80512b8016325e6f12a3dfbecb438d
Cassie Wang <cassie.wangy@gmail.com>
2024-07-28 22:18:04 +1200

    debug

--- client/apis/cakes.js
+++ client/apis/cakes.js
@@ -5,7 +5,7 @@ const rootUrl = '/api/v1'
 export function getCakes () {
   return request.get(rootUrl + '/cakes')
     .then(res => {
-      // console.log('api', res.body)
+      console.log('api', res.body)
       return res.body.cakes
     })
 }
