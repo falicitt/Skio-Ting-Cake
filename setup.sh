3173b4c76cb9a0e42f9115a39f3602e1f729b167
Cassie Wang <cassie.wangy@gmail.com>
2024-07-28 13:54:18 +1200

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-# Update package list
-apt-get update
-
-# Install Python and distutils
-apt-get install -y python3 python3-distutils
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    # Update package list and install dependencies on Ubuntu
+    apt-get update
+    apt-get install -y python3 python3-distutils
+else
+    echo "Unsupported OS: $OSTYPE"
+    exit 1
+fi
 
 # Proceed with npm install
 npm install
