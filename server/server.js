e31ad433c350b7391a1c3e7d5004282eb24df5d5
Cassie Wang <cassie.wangy@gmail.com>
2024-07-28 21:30:47 +1200

    connect to production database

--- server/server.js
+++ server/server.js
@@ -1,5 +1,28 @@
 const express = require('express')
 const path = require('path')
+const postgres = require('postgres')
+require('dotenv').config()
+
+const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env
+
+const sql = postgres({
+  host: PGHOST,
+  database: PGDATABASE,
+  username: PGUSER,
+  password: PGPASSWORD,
+  port: 5432,
+  ssl: 'require',
+  connection: {
+    options: `project=${ENDPOINT_ID}`
+  }
+})
+
+async function getPgVersion () {
+  const result = await sql`select version()`
+  console.log(result)
+}
+
+getPgVersion()
 
 const cakeRoutes = require('./routes/cakes')
 const ordersRoutes = require('./routes/orders')
